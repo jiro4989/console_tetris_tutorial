a092fcd96b3e90b412f1d1494c8938c4b94dd8ab
jiro4989 <jiroron666@gmail.com>
2020-06-25 00:20:12 +0000

    add tetris1

--- src/tetris1.nim
+++ src/tetris1.nim
@@ -1,5 +1,36 @@
-# This is just an example to get you started. A typical binary package
-# uses this file as the main entry point of the application.
+from os import sleep
+from strutils import join
+from sequtils import mapIt
 
-when isMainModule:
-  echo("Hello, World!")
+type
+  Board* = seq[seq[int]]
+
+const
+  initialBoard*: Board = @[
+    @[1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1],
+    @[1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1],
+    @[1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1],
+    @[1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1],
+    @[1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1],
+    @[1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1],
+    @[1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1],
+    @[1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1],
+    @[1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1],
+    @[1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1],
+    @[1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1],
+    @[1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1],
+    @[1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1],
+    @[1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1],
+    @[1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1],
+    @[1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1],
+    @[1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1],
+    @[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    @[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  ]
+
+while true:
+  for row in initialBoard:
+    # シーケンスの各要素を文字列に変換してから結合
+    echo row.mapIt($it).join
+  echo "---"
+  sleep 1000 # ミリ秒
